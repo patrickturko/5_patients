3414501dc9c74a0e20d78cfaa0f765a471472bcb
Patrick Turko <patrick.turko@gmail.com>
2017-10-05 11:07:11 +0200

    Reduced memory used by recalibrator

--- Mel_6_hs_metrics_cleanup_b37_170914.sh
+++ Mel_6_hs_metrics_cleanup_b37_170914.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-foo7 () {
-local i=$1
-	OUTPUT_DIR=`echo GATK_BQSR/`
-	NAME=`echo $i | sed 's/\([A-Za-z0-9_]*\)_sorted_dedup_fixmate.bam/\1/'`;
-	java -Xmx16G -jar /data/Phil/software/GATK_3.7/GenomeAnalysisTK.jar -T PrintReads -R /data/Phil/ref_phil/GATK_resource/b37/human_g1k_v37.fasta -I ${i} -BQSR ${NAME}_recal_data.table -o ${NAME}_sorted_dedup_fixmate_bqsr.bam 
-}
-export -f foo7
-for i in *sorted_dedup_fixmate.bam
-do
-sem -j 16 --id bqsr4 foo7 "$i"
-done
-sem --wait --id bqsr4
 
-rg_dir5=`echo QC_reports_b37`
-mkdir -p "$rg_dir5"
 
 foo8 () {
 local i=$1
