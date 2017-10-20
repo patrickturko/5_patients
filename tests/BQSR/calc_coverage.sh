eba4632a9a3ccf9dc09086e1d564eea0d6501a3e
Patrick Turko <patrick.turko@gmail.com>
2017-10-20 11:01:00 +0200

    Compared coverage of files which recalibrated successfully

--- tests/BQSR/calc_coverage.sh
+++ tests/BQSR/calc_coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd ../../../DNA/Exome/processed/2_skewer/test/4/
+cd ../../../DNA/Exome/processed/2_skewer/test/2/
 
 rg_dir=`echo coverage`
 mkdir -p "$rg_dir"
@@ -10,7 +10,7 @@ local i=$1
 	NAME=`echo $i | sed 's/\([A-Za-z0-9_]*\)_sorted_dedup_fixmate.bam/\1/'`;
 	echo ${NAME}
 	#samtools depth -b ../../../../intervals/S07604624_Regions.bed ${i} > ${i}.coverage
-	java -Xmx16G -jar /data/Phil/software/GATK_3.7/GenomeAnalysisTK.jar -T DepthOfCoverage -R /data/Phil/ref_phil/GATK_resource/b37/human_g1k_v37.fasta -I ${i} -L ../../../../intervals/targets.interval_list -o _coverage_stats
+	java -Xmx16G -jar /data/Phil/software/GATK_3.8/GenomeAnalysisTK.jar -T DepthOfCoverage -R /data/Phil/ref_phil/GATK_resource/b37/human_g1k_v37.fasta -I ${i} -L ../../../../intervals/melarray_b37.interval_list	 -o ${i}_coverage_stats.txt
 
 }
 export -f foo6
