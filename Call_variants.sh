37612e3908700b36d6193120b38b3b503ca75a2e
Patrick Turko <patrick.turko@gmail.com>
2017-10-31 12:30:01 +0100

    Added variant caller: Manta 1.2.1

--- Call_variants.sh
+++ Call_variants.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-cd ../DNA/Exome/processed/2_skewer/
 
-ls *blood*BQSR.bam | sed 's/\([Pp]atient_[[:digit:]]\).*/\1/' > patient_names.txt
+cd ../DNA/Exome/processed/2_skewer/test/6
+
+ls *blood*BQSR.bam | sed 's/.*\([Pp]atient_[[:digit:]]\).*/\1/' > patient_names.txt
 
 rg_dir5=`echo 7_variants_b37`
 mkdir -p "$rg_dir5"
@@ -10,46 +11,25 @@ mkdir -p "$rg_dir5"
 foo6 () {
 local tumor=$1
 local normal=$2
-local analysis_path=$3
-	NAME=`echo tumor | sed 's/\([A-Za-z0-9_]*\)_BQSR.bam/\1/'`;
+	NAME=`echo ${tumor} | sed 's/.*\([Pp]atient_[[:digit:]]\).*/\1/'`;
 	echo ${NAME}
-	/data/Phil/software/manta/bin/configManta.py --normalBam ${normal} --tumorBam ${tumor} --referenceFasta /data/Phil/ref_phil/GATK_resource/b37/human_g1k_v37.fasta --runDir ${pathway};
-
-	${analysis_path}/runWorkflow.py -m local -j 16	
+	mkdir ${NAME}
+	python /data/Phil/software/manta/configManta.py --normalBam ${normal} --tumorBam ${tumor} --referenceFasta /data/Phil/ref_phil/GATK_resource/b37/human_g1k_v37.fasta --exome --callRegions ../../../../intervals/S07604624_Covered.bed.gz --runDir $PWD;
+	
+	python runWorkflow.py -m local -j 8;
+	mkdir ${NAME}/python
+	mv *[Ww]orkflow* ${NAME}/python
+	mv results ${NAME}
+	mv workspace ${NAME}
+	
 
 
 }
-export -f foo4
+export -f foo6
 
-for word in $(cat test.txt)
+for word in $(cat patient_names.txt)
 do
-sem -j 16 --id manta foo6 ls *$word*tumor*BQSR.bam ls *$word*blood*BQSR.bam
+sem -j 16 --id manta foo6 *$word*tumor*BQSR.bam *$word*blood*BQSR.bam;
 done
 sem --wait --id manta
-
-
-
-
-rg_dir7=`echo MuTect2`
-mkdir -p "$rg_dir7"
-
-array=(`awk -F'\t' '{if (NR!=1) {print$2}}' Melarray_mutect2_tumor_normal.txt`)
-
-foo10 () {
-local i=$1
-array=(`awk -F'\t' '{if (NR!=1) {print$2}}' Melarray_mutect2_tumor_normal.txt`)
-array2=(`awk -F'\t' '{if (NR!=1) {print$3}}' Melarray_mutect2_tumor_normal.txt`)
-#printf "%s is in %s\n" "${array[i]}" "${array2[i]}"
-t1=`echo ${array[i]} | sed 's/\([A-Za-z0-9-]*\)\r/\1/'`
-n1=`echo ${array2[i]} | sed 's/\([A-Za-z0-9-]*\)\r/\1/'`
-mkdir ${t1}_${n1}
-java -Xmx16G -jar /data/Phil/software/GATK_3.6/GenomeAnalysisTK.jar -T MuTect2 -R /data/Phil/ref_phil/GATK_resource/hg19/ucsc.hg19.fasta -I:tumor ${t1}_sorted_dedup_fixmate_bqsr.bam -I:normal ${n1}_sorted_dedup_fixmate_bqsr.bam -o MuTect2/${t1}_${n1}/${t1}_mutect2.vcf --dbsnp /data/Phil/ref_phil/GATK_resource/hg19/dbsnp_138.hg19.vcf.gz --cosmic /data/Phil/ref_phil/GATK_resource/hg19/CosmicCodingMuts_chr_M_sorted.vcf.gz -L 151127_out.bed
-}
-export -f foo10
-
-for ((i=0;i<${#array[@]};++i))
-do
-sem --id mutect2 -j 20 foo10 "$i"
-done
-sem --wait --id mutect2
 
--- GATK_4_BQSR.sh
+++ GATK_4_BQSR.sh
@@ -2,7 +2,17 @@
 
 cd /data2/patrick/Patients/DNA/Exome/processed/2_skewer/test/5
 
-
+foo4 () {
+local i=$1
+	NAME=`echo $i | sed 's/\([A-Za-z0-9_]*\)_sorted_dedup_fixmate.bam/\1/'`;
+	/data/Phil/software/GATK4B5/gatk-launch --javaOptions "-Xmx64G" BaseRecalibrator -R /data/Phil/ref_phil/GATK_resource/b37/human_g1k_v37.fasta -I ${i} -L ../../../../intervals/targets.interval_list --interval_padding 100 --knownSites /data/Phil/ref_phil/GATK_resource/b37/dbsnp_138.b37.vcf.gz  --knownSites /data/Phil/ref_phil/GATK_resource/b37/Mills_and_1000G_gold_standard.indels.b37.vcf.gz --knownSites /data/Phil/ref_phil/GATK_resource/b37/1000G_phase3_v4_20130502.sites.vcf.gz -O ${NAME}_recal_data.table
+	}
+export -f foo4
+for i in *sorted_dedup_fixmate.bam
+do 
+sem -j 16 --id bqsr1 foo4 "$i"
+done
+sem --wait --id bqsr1
 
 
 foo5 () {
